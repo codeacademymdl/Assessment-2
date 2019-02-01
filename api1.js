02a614fd1ea222a0f87f800469e0bfa77584fa09
vibhore.vg <vibhore97@gmail.com>
2019-02-01 11:56:37 +0530

    api1.js:fetch ratings from api2 working

--- api1.js
+++ api1.js
@@ -1,16 +1,33 @@
 const https = require('https');
 
 // const apiList = [];
-//
+// promise.race
+const ratingID = [];
 const getRequest = () => {
   const promise = new Promise((resolve, reject) => {
     const apiURL = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks';
-    return https.get(apiURL, (res) => {
+    return https.get(apiURL, (response) => {
       let data = '';
-      res.on('data', (chunk) => {
+      response.on('data', (chunk) => {
         data += chunk;
       });
-      res.on('end', () => {
+      response.on('end', () => {
+        resolve(data);
+      });
+    });
+  });
+  return promise;
+};
+
+const getRating = (bookID) => {
+  const promise = new Promise((resolve, reject) => {
+    const apiURL = `https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${bookID}`;
+    return https.get(apiURL, (response) => {
+      let data = '';
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
+      response.on('end', () => {
         resolve(data);
       });
     });
@@ -19,7 +36,25 @@ const getRequest = () => {
 };
 
 
-// const booksJson = getRequest().then(data => JSON.parse(data));
+const booksJson = getRequest().then((data) => {
+  const books = JSON.parse(data).books;
+  // console.log(books[11]);
+  for (let i = 0; i < books.length; i += 1) {
+    ratingID.push(books[i].id);
+  }
+  if (ratingID.length > 0) { return ratingID; }
+}).then((ratingID) => {
+  const promiseIDRatingRequest = [];
+  ratingID.forEach((value) => {
+    promiseIDRatingRequest.push(getRating(value));
+  });
+  const multiplePromiseCalls = async () => {
+    const result = await Promise.all(promiseIDRatingRequest);
+    console.log(result);
+    // return result;
+  };
+  multiplePromiseCalls();
+});
 
 
-module.exports = getRequest;
+module.exports = { getRequest, getRating };
